6bbd747d1a0296744e4f967b322e070b20cf19cf
Serhii Kharsun <serhii.kharsun@ilogos.group>
2026-07-10 00:05:30 +0300

    [FM-10] change access modification

--- Assets/Presentation/UI/DistrictBuild/Components/DistrictBuildSelectionComponent.cs
+++ Assets/Presentation/UI/DistrictBuild/Components/DistrictBuildSelectionComponent.cs
@@ -6,7 +6,7 @@ namespace Presentation.UI.DistrictBuild.Components
     // single entity tagged DistrictBuildSelectionTag, created on overlay-open and destroyed on close by
     // DistrictBuildUISystem. Written exclusively by DistrictBuildListUISubSystem (default-selected on open, then on
     // each row click), read + reconciled by the other section subsystems. Not visible outside this subdomain.
-    public struct DistrictBuildSelectionComponent
+    internal struct DistrictBuildSelectionComponent
     {
         public DistrictType Selected;
     }
